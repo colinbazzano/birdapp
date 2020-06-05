f2b0235638ed3b6dfe3e28d5719aece7cf76d713
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-06-05 13:07:39 -0400

    creates a tweet list view to be consumed by JS

--- tweets/views.py
+++ tweets/views.py
@@ -7,7 +7,22 @@ from .models import Tweet
 
 
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    consume by JavaScript
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+    return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
