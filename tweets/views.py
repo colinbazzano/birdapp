cef662c3adfb7bf13aad210860aeca0d78739383
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-06-05 12:40:48 -0400

    can load tweets onto dynamic page ids

--- tweets/views.py
+++ tweets/views.py
@@ -1,6 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+# we do .models because we are in the directory with tweet in it
+from .models import Tweet
 # Create your views here.
 
 
@@ -9,5 +11,27 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+    # print(args, kwargs)
+    """
+    REST API VIEW
+    consume by JavaScript
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+        # "content": obj.content
+        # "image_path": obj.image.url
+    }
+    status = 200
+    try:
+        # get the tweet with the specified id
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    # obj.content is from our models. it's the tweet's content
+
+    # json.dumps content_type='application/json'
+    return JsonResponse(data, status=status)
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
