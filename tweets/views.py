317b3766036583587ab3cd3656a1d3473e73e87d
Colin Bazzano <colinbazzano@Colins-MacBook-Pro.local>
2020-06-08 13:09:57 -0400

    styling tweaks

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -18,7 +19,8 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content,
+                    "likes": random.randint(0, 1000)} for x in qs]
     data = {
         "response": tweets_list
     }
